efd5a5b15ae8e9e94067cd85bb99cc7f956c3b0c
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-12-07 15:40:54 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,12 @@ user1 = User.create!(fullname: "Muriithi", email: "mk@gmail.com", password: "mkj
 user2 = User.create!(fullname: "Jane", email: "Wandia@gmail.com", password: "JaneWandia", password_confirmation: "JaneWandia")
 user3 = User.create!(fullname: "Chelah", email: "Chelah@gmail.com", password: "12345", password_confirmation: "12345")
 
-airline1 = Airline.create(name: "Qatar", image: "https://ibb.co/TtZpGhz")
-airline2 = Airline.create(name: "Emirates Airlines", image: "https://ibb.co/1fKCP3D")
-airline3 = Airline.create(name: "Kenya Airways", image: "https://ibb.co/7169y8b")
-airline4 = Airline.create(name: "KLM", image: "https://ibb.co/gSvWzMz")
-airline5 = Airline.create(name: "Ethopian Airlines", image: "https://ibb.co/BrB9hrw")
-airline6 = Airline.create(name: "American Airlines", image: "https://ibb.co/DVnTC8c")
+airline1 = Airline.create(name: "Qatar", image: "https://upload.wikimedia.org/wikipedia/commons/b/b2/Airbus_A380_Qatar_Airways.jpg")
+airline2 = Airline.create(name: "Emirates Airlines", image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Emirates_A6-EWA_Boeing_777-200LR_taking_off_from_LAX_%285222343985%29.jpg")
+airline3 = Airline.create(name: "Kenya Airways", image: "https://upload.wikimedia.org/wikipedia/commons/5/5f/5Y-KZE_%28aircraft%29_on_final_at_Schiphol_runway_18R_pic3.JPG")
+airline4 = Airline.create(name: "KLM", image: "https://upload.wikimedia.org/wikipedia/commons/5/5e/PH-BGU.jpg")
+airline5 = Airline.create(name: "Ethopian Airlines", image: "https://upload.wikimedia.org/wikipedia/commons/f/f8/Ethiopian_Airlines_A350_ET-ATQ.jpg")
+airline6 = Airline.create(name: "American Airlines", image: "https://upload.wikimedia.org/wikipedia/commons/c/c8/N723AN_Boeing_777_American_Airlines_%289483566883%29.jpg")
 
 Review.create!(trip: "Kenya to Dubai", review: "The flight was great. Plane had nice tv screens, and the food was amazing.", rating: 4, airline_id: airline2.id, user_id: user1.id)
 Review.create!(trip: "Nairobi to India", review: "The flight was ok: planes tv screens were quite small, but the food was decent.", rating: 3, airline_id: airline3.id, user_id: user2.id)
